693b220cb73854637e1108d52b47450f67f00ff2
Primož Hrovat <primoz.hrovat@ap-sol.eu>
2022-11-10 16:00:25 +0100

    initial commit - test

---
+++ src/broker/broker.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { MemphisModule } from 'memphis-dev/nest';
+import { BrokerService } from './broker.service';
+
+@Module({ imports: [MemphisModule.register()], exports: [BrokerService], providers: [BrokerService], })
+export class BrokerModule {}
